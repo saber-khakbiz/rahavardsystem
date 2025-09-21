4b482eefe983abee4b5405165fd94d6a16c0af75
Saber Khakbiz <saberkhakbiz73@gmail.com>
2025-09-21 22:24:39 +0330

    add javascript code to project

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.getElementById("typing");
+  const text = "پیمان ایمن رهاورد سیستم";
+  let index = 0;
+  let isDeleting = false;
+  const speed = 100;   // سرعت تایپ
+  const delay = 2000;  // مکث بعد از نوشتن کامل
+
+  function typeEffect() {
+    if (!isDeleting && index <= text.length) {
+      element.textContent = text.substring(0, index++);
+      setTimeout(typeEffect, speed);
+    } else if (isDeleting && index >= 0) {
+      element.textContent = text.substring(0, index--);
+      setTimeout(typeEffect, speed / 2);
+    } else {
+      isDeleting = !isDeleting;
+      setTimeout(typeEffect, delay);
+    }
+  }
+
+  typeEffect();
+});
